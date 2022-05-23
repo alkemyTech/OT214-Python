0fbbe93760ed98013cf764cce68f8f2d6ba4efbc
Saul Rincon <saulrincon@192.168.1.6>
2022-05-23 20:55:03 -0300

    flake8 corrections

--- universidad_c/dags/university_c_dag.py
+++ universidad_c/dags/university_c_dag.py
@@ -1,43 +1,45 @@
+from datetime import timedelta, datetime
+
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-
-from datetime import timedelta, datetime 
-
 
 # DAG default arguments dictionary, running hourly with 5 retries.
 default_args = {
     'owner': 'alkimer',
-    'schedule_interval':'@hourly',
+    'schedule_interval': '@hourly',
     'retries': 5,
     'retry_delay': timedelta(seconds=30),
-    'tags':'[aceleracion]'
+    'tags': '[aceleracion]'
 }
+
 
 # Function to Extract data from source DataBase.
 def extract_data():
     pass
-        
-# Function to Transform the data extracted. 
-def transform_data():    
+
+
+# Function to Transform the data extracted.
+def transform_data():
     pass
+
 
 # Function to upload the data to S3.
 def upload_data():
     pass
-    
-        
+
+
 # DAG to execute the ETL on schedule. Dict with default_args injected.
 with DAG(
     'university_C',
     start_date=datetime.today(),
     description='ETL for universities: Palermo, Jujuy',
-    default_args=default_args,    
+    default_args=default_args,
 ) as dag:
 
     # PythonOperator to execute the extract_data function.
     opr_extract_data = PythonOperator(
             task_id='extract_data',
-            python_callable = extract_data,
+            python_callable=extract_data,
     )
 
     # PythonOperator to execute the transform_data function.
