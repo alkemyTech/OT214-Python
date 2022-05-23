02cca9da2ed78359509d69b669368f171f60505f
Maximiliano <maxicabrera23@gmail.com>
2022-05-22 21:54:30 -0300

    univeristy_queries_by_date_add_code_ulapampa

---
+++ universidades_e/sql/nacional_de_la_pampa.sql
@@ -0,0 +1,16 @@
+SELECT 
+	universidad, 
+	carrerra, 
+	fechaiscripccion, 
+	nombrre, 
+	sexo,
+	nacimiento, 
+	codgoposstal, 
+	direccion, 
+	eemail
+FROM 
+	moron_nacional_pampa 
+WHERE 
+	to_date(fechaiscripccion,'DD-MM-YYYY') BETWEEN '2020-09-01' AND '2021-02-01' 
+AND 
+	universidad='Universidad nacional de la pampa';
