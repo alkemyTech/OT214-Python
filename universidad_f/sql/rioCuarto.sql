f7a00e63f0016b339d4e9c4ddd49bca8f89ceaa5
Sebastian Manassero <35856008+zedeme@users.noreply.github.com>
2022-05-23 18:48:09 -0300

    University-F SQL queries added (#3)

---
+++ universidad_f/sql/rioCuarto.sql
@@ -0,0 +1,12 @@
+SELECT univiersities, 
+       carrera, 
+       inscription_dates, 
+       names, 
+       sexo, 
+       fechas_nacimiento, 
+       localidad, 
+       direcciones, 
+       email 
+
+FROM rio_cuarto_interamericana
+WHERE univiersities LIKE 'Universidad-nacional-de-río-cuarto' AND TO_DATE(inscription_dates, 'YY/Mon/DD') BETWEEN TO_DATE('20/Sep/01', 'YY/Mon/DD') AND TO_DATE('21/Feb/01', 'YY/Mon/DD');
