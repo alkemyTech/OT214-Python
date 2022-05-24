4d5327ea29f01c1bb21a22421fe8b496e88e8c7f
zedeme <sebasmanassero@gmail.com>
2022-05-24 20:54:58 -0300

    Retries and database connection added

--- universidad_f/dags/university_f.py
+++ universidad_f/dags/university_f.py
@@ -1,12 +1,33 @@
 from datetime import timedelta
+from os import getenv
+
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists
 
 default_args = {
     "owner": "alkymer",
     "depends_on_past": False,
-    "email": ["example@example.com"],
     "email_on_failure": False,
     "email_on_retry": False,
     "retries": 5,
     "retry_delay": timedelta(minutes=5),
     "schedule_interval": '@hourly'
 }
+
+
+class ETL():
+    def _database_status(self, engine):
+        if not database_exists(engine) or not engine:
+            return False
+        return True
+
+    def extract(self):
+        database = getenv("DATABASE_CONNECTION")
+        engine = create_engine("postgresql://" + database.USER +
+                               ":" + database.PASSWORD +
+                               "@" + database.HOST +
+                               ":" + database.PORT +
+                               "/" + database.DATABASE)
+
+        if not self._database_status(engine):
+            raise ValueError("Database doesn't exists")
