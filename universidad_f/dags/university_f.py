518f664198ff136b56bd8e720e6177c3d261e22b
zedeme <sebasmanassero@gmail.com>
2022-05-26 20:01:12 -0300

    PR Fixes added

--- universidad_f/dags/university_f.py
+++ universidad_f/dags/university_f.py
@@ -1,8 +1,9 @@
 from datetime import timedelta
-from os import getenv
 
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists
+
+from decouple import config
 
 default_args = {
     "owner": "alkymer",
@@ -17,17 +18,16 @@ default_args = {
 
 class ETL():
     def _database_status(self, engine):
-        if not database_exists(engine) or not engine:
+        if not database_exists(engine) or not bool(engine):
             return False
         return True
 
     def extract(self):
-        database = getenv("DATABASE_CONNECTION")
-        engine = create_engine("postgresql://" + database.USER +
-                               ":" + database.PASSWORD +
-                               "@" + database.HOST +
-                               ":" + database.PORT +
-                               "/" + database.DATABASE)
+        engine = create_engine("postgresql://" + config("USER") +
+                               ":" + config("PASSWORD") +
+                               "@" + config("HOST") +
+                               ":" + config("PORT") +
+                               "/" + config("DATABASE"))
 
         if not self._database_status(engine):
             raise ValueError("Database doesn't exists")
