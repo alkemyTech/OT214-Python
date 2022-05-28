509581d44b1a4019ac910177d878e599db678107
Matias Pariente <matiaspariente@hotmail.com>
2022-05-28 11:37:32 -0300

    feature: SQL extract Operator from Universidades Flores / Villa Maria

---
+++ universidades_a/dags/universidades_a.py
@@ -0,0 +1,54 @@
+import logging
+from pathlib import Path
+
+import pandas as pd
+from decouple import config
+from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
+
+
+def extract_sql():
+
+    try:
+        engine = create_engine(
+            "postgresql://{}:{}@{}:{}/{}"
+            .format(
+                config('_PG_USER'),
+                config('_PG_PASSWD'),
+                config('_PG_HOST'),
+                config('_PG_PORT'),
+                config('_PG_DB')))
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        if "port" in error:
+            logging.critical("Comunication Error, verify HOST:PORT")
+        else:
+            logging.critical("Autentication error, verify User / password")
+    else:
+        logging.info("Database connection success")
+
+    try:
+        filepath_flores = Path('../sql/flores.sql')
+        filepath_villamaria = Path('../sql/villaMaria.sql')
+        with open(filepath_flores, 'r', encoding="utf-8") as file:
+            query_flores = file.read()
+        with open(filepath_villamaria, 'r', encoding="utf-8") as file:
+            query_villamaria = file.read()
+        df_flores = pd.read_sql(query_flores, engine)
+        df_villamaria = pd.read_sql(query_villamaria, engine)
+    except IOError:
+        logging.error("SQL file not appear to exist")
+    else:
+        logging.info("SQL query reading success")
+
+    try:
+        filepath_flores_csv = Path('files/universidad_flores.csv')
+        filepath_flores_csv.parent.mkdir(parents=True, exist_ok=True)
+        filepath_villamaria_csv = Path('files/universidad_villamaria.csv')
+        filepath_villamaria_csv.parent.mkdir(parents=True, exist_ok=True)
+        df_flores.to_csv(filepath_flores_csv, index=False)
+        df_villamaria.to_csv(filepath_villamaria_csv, index=False)
+    except Exception as exc:
+        logging.error(exc)
+    else:
+        logging.info("csv files were generated successfully")
