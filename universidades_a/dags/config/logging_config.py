74cd15a757f37d360713e44c21a8bf7a641ac68b
Matias Pariente <matiaspariente@hotmail.com>
2022-05-28 16:19:18 -0300

    feature: Logger config from universidad de Flores / Villa Maria - Paths changed and DocString Added

--- universidades_a/dags/config/logging_config.py
+++ universidades_a/dags/config/logging_config.py
@@ -1,15 +1,23 @@
 import logging
 import logging.config
+from pathlib import Path
 
 import yaml
 
 
-# Logging setup , the configuration parameters are taken
-# from the yaml file that works as a dictionary
 def setup_logging(default_level=logging.INFO):
+    '''
+    Logging setup , the configuration parameters are taken
+    from the yaml file that works as a dictionary
+
+        Returns:
+                    (str): configuration result message
+    '''
 
     try:
-        with open("./config/logging_config.yaml", 'rt') as file:
+        logging_config_path = Path(
+            'airflow/universidades_a/dags/config/logging_config.yaml')
+        with open(logging_config_path, 'rt') as file:
             config = yaml.safe_load(file.read())
             logging.config.dictConfig(config)
             return ('Configuracion de Logging exitosa')
