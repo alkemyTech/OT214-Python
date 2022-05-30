dcabd6e204f02a291e4e1880c063591e05e19193
Maximiliano <maxicabrera23@gmail.com>
2022-05-30 00:05:44 -0300

    final_fix_flake8

--- universidades_e/dags/university_e_dags.py
+++ universidades_e/dags/university_e_dags.py
@@ -2,14 +2,14 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
 
-#dag default arguments
+# dag default arguments
 default_arguments = {
-    'owner' : 'Maxi Cabrera',
-    'start_date' : days_ago(1)
+    'owner': 'Maxi Cabrera',
+    'start_date': days_ago(1)
 }
 
 
-#dag start run every hour 
+# dag start run every hour
 with DAG(
     dag_id='create_dag_structure',
     schedule_interval='@hourly',
@@ -17,46 +17,36 @@ with DAG(
     default_args=default_arguments,
 )as dag:
 
-
-#function of connection and db queries
+    # function of connection and db queries
     def conn_query():
         print('generate a connection whith db')
         print('national university of la pampa querie')
         print('inter-american open university querie')
 
-
-#process data function
+    # process data function
     def proccess():
         print('process data whith pandas')
 
-
-#function to send data process to s3
+    # function to send data process to s3
     def send():
         print('send of panda processed info to a3 server')
 
-
-#pythonoperator for function of connect and queries
+    # pythonoperator for function of connect and queries
     python_task1 = PythonOperator(
         task_id="connect_queries",
         python_callable=conn_query,
-
     )
 
-
-#pythonoperator for function to process data
+    # pythonoperator for function to process data
     python_task2 = PythonOperator(
         task_id="process",
         python_callable=proccess,
-
     )
 
-
-#pythonoperator for function to send data to s3
+    # pythonoperator for function to send data to s3
     python_task3 = PythonOperator(
         task_id="send_to_up_cloud",
         python_callable=send,
-
     )
 
 python_task1 >> python_task2 >> python_task3
-
