8d32666d34980780df1de35849075675932fa315
FabianaTM <fabianatorresmeza@gmail.com>
2022-05-30 12:24:35 -0300

    Feature: logger_config-OT214-45. Files .py and .yaml

---
+++ universidades_b/dags/logging_config.py
@@ -0,0 +1,15 @@
+import logging
+
+import yaml
+
+import os
+
+def config():
+    root_path = os.path.dirname(__file__)
+    config_path = os.path.join(root_path,'logging_config.yaml')
+    with open(config_path) as file:
+        config = yaml.safe_load(file.read())
+        logging.config.dictConfig(config)
+        logger = logging.getLogger(__name__)
+
+    return logger
