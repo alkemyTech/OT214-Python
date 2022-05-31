329a8bea52e758fb38e5af0911490d8038e406bc
zedeme <sebasmanassero@gmail.com>
2022-05-31 14:47:58 -0300

    Queries re-added

---
+++ universidad_f/sql/moron.sql
@@ -0,0 +1,12 @@
+  
+SELECT universidad, 
+       carrerra, 
+       fechaiscripccion, 
+       nombrre, 
+       sexo, 
+       nacimiento, 
+       codgoposstal, 
+       direccion, 
+       eemail
+FROM moron_nacional_pampa
+WHERE universidad LIKE 'Universidad de morón' AND TO_DATE(fechaiscripccion, 'DD/MM/YYYY') BETWEEN TO_DATE('01/09/2020', 'DD/MM/YYYY') AND TO_DATE('01/02/2021', 'DD/MM/YYYY');
