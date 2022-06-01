77c8c3d60a34a6d9a30f9ba3331d8d605b39e29f
zedeme <sebasmanassero@gmail.com>
2022-06-01 01:56:11 -0300

    re-make exceptions, change logging's level exceptions

--- universidad_f/dags/config_loader.py
+++ universidad_f/dags/config_loader.py
@@ -2,23 +2,30 @@ import logging
 import logging.config
 import yaml
 
+configuration_path = "./config/"
 
-def get_logger(logger="dev"):
+
+def _getting_default_logger():
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    return logger
+
+
+def get_logger(logger_file="logger.yaml", logger_name="dev"):
     try:
-        with open("./config/logger.yaml") as logger_config:
+        with open(configuration_path + logger_file) as logger_config:
             try:
                 log_config = yaml.safe_load(logger_config.read())
                 logging.config.dictConfig(log_config)
-            except Exception as e:
-                logging.info("Error loading logger configuration \n" + str(e))
-                logging.basicConfig(level=logging.INFO)
+            except Exception:
+                logging.exception("Error loading logger configuration")
+                return _getting_default_logger()
             finally:
                 logger_config.close()
-    except Exception as e:
-        logging.info("Error, logger's file configuration not found \n" +
-                     str(e))
-        logging.basicConfig(level=logging.INFO)
+    except Exception:
+        logging.exception("Error, logger's file configuration not found")
+        return _getting_default_logger()
 
-    logger = logging.getLogger(logger)
+    logger = logging.getLogger(logger_name)
 
     return logger
