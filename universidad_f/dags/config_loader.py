abb52380e7c12a2181d6f2a80bfd253eee93330b
Sebastian Manassero <35856008+zedeme@users.noreply.github.com>
2022-06-10 03:13:21 -0300

    feature: Pandas operator added (#31)
    * Pandas operator added
    
    * PR Fixed: import fixes
    
    * pandas operator added
    
    * PR Fixes: .env-template added re-order paths
    
    * Merge remote-tracking branch 'origin/main' into feature-pandas_operator-OT214-81
    
    Co-authored-by: julianpalmerio <julianpalmerio@hotmail.com>

--- universidad_f/dags/config_loader.py
+++ universidad_f/dags/config_loader.py
@@ -7,6 +7,19 @@ import yaml
 configuration_path = os.path.dirname(__file__) + "/config/"
 
 
+def get_dataframe_config(columns_file="columns.yaml"):
+    try:
+        with open(configuration_path + columns_file) as column_config:
+            df_config = yaml.safe_load(column_config.read())
+
+    except Exception as e:
+        logging.exception("Error loading dataframe's configuration" + str(e))
+
+    finally:
+        column_config.close()
+        return df_config["dataframes_config"]
+
+
 def _getting_default_logger():
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
