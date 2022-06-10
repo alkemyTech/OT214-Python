460d70e440044d9d6850e8e760dfd6601e10d84e
matiaspariente <54297169+matiaspariente@users.noreply.github.com>
2022-06-10 01:26:16 -0300

    feature: PythonOperator Config from Universidad de Flores / Villa Maria (#28)
    * feature: PythonOperator Config from Universidad de Flores / Villa Maria
    
    * feature: PythonOperator Config from Universidad de Flores / Villa Maria - fix: Logging
    
    Co-authored-by: julianpalmerio <julianpalmerio@hotmail.com>

--- universidades_a/dags/universidades_a.py
+++ universidades_a/dags/universidades_a.py
@@ -80,10 +80,41 @@ def extract_sql():
         logger.info("csv files were generated successfully")
 
 
-def transform_data():
-
-    # function that will process data with Pandas
+def transform_flores():
+    '''
+    Function that will normalize the data of the Universidad de Flores.
+    Will return the final state of normalization
+    '''
     pass
+
+
+def transform_villamaria():
+    '''
+    Function that will normalize the data of the Universidad de Villa Maria.
+    Will return the final state of normalization
+    '''
+    pass
+
+
+def transform_data():
+    '''
+    Universities Data is extracted from the csv files generated in extract task
+    These data are normalized as required through the transform functions
+    '''
+    logger = logging.getLogger("Transform")
+    res = transform_flores()
+    if(res == "success"):
+        logger.info(
+            "Universidad de Flores normalized txt were generate succesfully")
+    else:
+        logger.error(res)
+    res = transform_villamaria()
+    if(res == "success"):
+        logger.info(
+            "Universidad de Villa María"
+            "normalized txt were generate succesfully")
+    else:
+        logger.error(res)
 
 
 def load_data_flores():
