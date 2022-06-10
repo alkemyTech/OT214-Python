30426b1c17c086175eb4f0b83732442acdcf65f7
Saul Rincon <98064909+piernamala@users.noreply.github.com>
2022-06-10 02:40:10 -0300

    Pandas functions to perform extraction and transformation for univers… (#29)
    * Pandas functions to perform extraction and transformation for university_c created inside dag file, log configuration, files folder created
    
    * Code fixes, missing dictionary
    
    * flake8 check
    
    * chore: delete files
    
    Co-authored-by: Saul Rincon <saulrincon@192.168.1.6>
    Co-authored-by: julianpalmerio <julianpalmerio@hotmail.com>

---
+++ universidades_c/dags/logging_config.py
@@ -0,0 +1,15 @@
+import logging
+import logging.config
+from pathlib import Path
+
+import yaml
+
+
+def logging_configuration():
+    path = Path('./OT214-Python/universidades_c/dags/config/logging_config.yaml')
+    with open(path, 'rt') as f:
+        config = yaml.safe_load(f.read())
+        logging.config.dictConfig(config)
+        logger = logging.getLogger('__name__')
+
+    return logger
---
+++ universidades_c/dags/pandas_processing_dicts.py
@@ -0,0 +1,23 @@
+
+palermo_columns = {
+                    'universidad': 'university',
+                    'careers': 'career',
+                    'fecha_de_inscripcion': 'inscription_date',
+                    'names': 'names',
+                    'sexo': 'gender',
+                    'birth_dates': 'age',
+                    'codigo_postal': 'postal_code',
+                    'direcciones': 'location',
+                    'correos_electronicos': 'email'
+}
+
+jujuy_columns = {
+                    'university': 'university',
+                    'career': 'career',
+                    'inscription_date': 'inscription_date',
+                    'sexo': 'gender',
+                    'birth_date': 'age',
+                    'direccion': 'location',
+                    'email': 'email',
+                    'codigo_postal': 'postal_code'
+}
